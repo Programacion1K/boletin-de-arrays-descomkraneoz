b543dba093b604a40c93e57190f06f4f2159962d
Manuel Martinez <descom666999@gmail.com>
2019-01-11 23:12:59 +0100

    @migueldavidorrico sin terminar los metodos

--- src/DireccionIP.java
+++ src/DireccionIP.java
@@ -1,54 +1,80 @@
 public class DireccionIP {
+    int[] direccionIPv4;
 
-    private int primerEnteroIP;
-    private int segundoEnteroIP;
-    private int tercerEnteroIP;
-    private int cuartoEnteroIP;
-    private int [] arrayIP;
-    private String cadenaIP;
+        //https://es.wikipedia.org/wiki/Direcci%C3%B3n_IP
 
-    public DireccionIP(int primerEntero, int segundoEntero, int tercerEntero, int cuartoEntero) {
-        this.primerEnteroIP = primerEntero;
-        this.segundoEnteroIP = segundoEntero;
-        this.tercerEnteroIP = tercerEntero;
-        this.cuartoEnteroIP = cuartoEntero;
-    }
+    DireccionIP mascaraRed;
 
-    public  DireccionIP(String cadena){
-        this.cadenaIP=cadena;
-    }
+    DireccionIP idRed;
 
-    public DireccionIP(int[] ArrayIP) {
-        arrayIP[0]=primerEnteroIP;
-        arrayIP[1]=segundoEnteroIP;
-        arrayIP[2]=tercerEnteroIP;
-        arrayIP[3]=cuartoEnteroIP;
+    private static final String CLASE="Clase";
+    private static final String MASCARA_RED="Máscara de Red";
+    private static final String BROADCAST="Id. Broadcast de clase";
+    private static final String A=" A";
+    private static final String B=" B";
+    private static final String C=" C";
 
-    }
 
-    public String imprimeSalidaEnCadena() {
 
-        return cadenaIP;
-    }
-
-    public String imprimeSalidaDeEnteros() {
-
-        return primerEnteroIP+"."+segundoEnteroIP+"."+tercerEnteroIP+"."+cuartoEnteroIP;
-    }
-
-    public String imprimeSalida(){
-        String salida="";
-
-        if (salida==null){
-            return imprimeSalidaEnCadena();
-        }else{
-            return imprimeSalidaDeEnteros();
-        }
-
+    DireccionIP(int octeto1,int octeto2,int octeto3,int octeto4){
+        this.direccionIPv4=new int[4];
+        this.direccionIPv4[0]=octeto1;
+        this.direccionIPv4[1]=octeto2;
+        this.direccionIPv4[2]=octeto3;
+        this.direccionIPv4[3]=octeto4;
+        //this.mascaraRed=new DireccionIP(255,255,255,0); //No es asi
+        //this.idRed=new DireccionIP(this.direccionIPv4[0],this.direccionIPv4[1],this.direccionIPv4[2],this.direccionIPv4[3]);
     }
 
     @Override
     public String toString() {
-        return imprimeSalida();
+        return this.direccionIPv4[0]+"."+this.direccionIPv4[1]+"."+this.direccionIPv4[2]+"."+this.direccionIPv4[3];
     }
+
+    private static final int VALOR_CLASE_A=127;
+    private static final int VALOR_CLASE_B=191;
+    private static final int VALOR_CLASE_C=223;
+    private static final int VALOR_MASCARA_RED=255;
+
+
+
+    public static String dimeClase(DireccionIP miIP) {
+
+        if (miIP.direccionIPv4[0]<=VALOR_CLASE_A){
+            return CLASE+A;
+        }
+        else if (miIP.direccionIPv4[0]<=VALOR_CLASE_B){
+            return CLASE+B;
+        }
+        else if (miIP.direccionIPv4[0]<=VALOR_CLASE_C){
+            return CLASE+C;
+        }
+        return "IP no perteneciente a ninguna clase";
+    }
+
+    public static String dimeMascaraRed(DireccionIP miIP){
+        if(miIP.direccionIPv4[0]==VALOR_MASCARA_RED && miIP.direccionIPv4[1]==0 && miIP.direccionIPv4[2]==0 && miIP.direccionIPv4[3]==0){
+            return MASCARA_RED+A;
+        }
+        else if(miIP.direccionIPv4[0]==VALOR_MASCARA_RED && miIP.direccionIPv4[1]==VALOR_MASCARA_RED && miIP.direccionIPv4[2]==0 && miIP.direccionIPv4[3]==0){
+            return MASCARA_RED+B;
+        }
+        else if(miIP.direccionIPv4[0]==VALOR_MASCARA_RED && miIP.direccionIPv4[1]==VALOR_MASCARA_RED && miIP.direccionIPv4[2]==VALOR_MASCARA_RED && miIP.direccionIPv4[3]==0){
+            return MASCARA_RED+C;
+        }
+        else if(miIP.direccionIPv4[3]==VALOR_MASCARA_RED && miIP.direccionIPv4[2]==VALOR_MASCARA_RED && miIP.direccionIPv4[1]==VALOR_MASCARA_RED){
+            return BROADCAST+A;
+        }
+        else if(miIP.direccionIPv4[3]==VALOR_MASCARA_RED && miIP.direccionIPv4[2]==VALOR_MASCARA_RED){
+            return BROADCAST+B;
+        }
+        else if(miIP.direccionIPv4[3]==VALOR_MASCARA_RED){
+            return BROADCAST+C;
+        }
+        return "Máscara de red no perteneciente a ninguna clase";
+    }
+
+
+
+
 }
--- src/PruebaIPs.java
+++ src/PruebaIPs.java
@@ -1,27 +1,13 @@
-import java.util.Arrays;
-
 public class PruebaIPs {
     public static void main(String[] args) {
 
-        DireccionIP cadenaIP=new DireccionIP("192.168.0.50");
+        DireccionIP mia=new DireccionIP(192,168,33,0);
+        System.out.println(mia);
 
-        System.out.println(cadenaIP.imprimeSalidaEnCadena());
-
-        DireccionIP enterosIP=new DireccionIP(10,0,0,3);
-
-        System.out.println(enterosIP.imprimeSalidaDeEnteros());
-
-        DireccionIP matrizIP[]={new DireccionIP(127,25,25,30)};
-
-        for (int i = 0; i <matrizIP.length ; i++) {
-            System.out.println(Arrays.toString(matrizIP));
-        }
-
-
-
-
-
+        System.out.println(DireccionIP.dimeClase(mia));
 
 
     }
+
+
 }
